c7446f3f60d8597bced22ae70c9ffccf84c1e35f
Tikush <46529368+Tikush@users.noreply.github.com>
2021-07-08 02:22:28 +0400

    Dev/share location map (#5)
    * - add map in share storyboard
    
    * - add share button

--- TeamWork-Location/Presentation/Scenes/Share/View/ShareViewController.swift
+++ TeamWork-Location/Presentation/Scenes/Share/View/ShareViewController.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
+enum Constant {
+    static let copied = "Copied"
+    static let completed = "completed"
+    static let cencalled = "cencaled"
+    static let presented = "presented"
+    static let failed = "failed"
+    static let sent = "sent"
+    static let inviteMessage = "I've been using TransferWise to send money abroad. really easy and a lot cheaper than the bank! I got you a free transfer using my invite link: "
+    static let link = "https://transferwise.com/invite/is/e6ecea"
+}
 
 class ShareViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func shareLocation(_ sender: Any) {
+        let activityController = UIActivityViewController(activityItems: [" "], applicationActivities:  nil)
+        activityController.completionWithItemsHandler = { (nil, completed, _, error) in
+            completed ? print(Constant.completed) : print(Constant.cencalled)
+        }
+        present(activityController, animated: true) {
+            print(Constant.presented)
+        }
+    }
 }

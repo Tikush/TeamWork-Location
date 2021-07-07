fe82f00a47f797642d976a15c09168595dc2234c
lado at luka <tsivtsivadzelado@yahoo.com>
2021-07-07 05:53:57 -0700

    added map view and filling view model class

--- TeamWork-Location/Presentation/Scenes/Routers/VIew /RoutersViewController.swift
+++ TeamWork-Location/Presentation/Scenes/Routers/VIew /RoutersViewController.swift
@@ -6,10 +6,23 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class RoutersViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    private var routersViewModel: RoutersViewModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configViewModel()
+    }
+    
+    func configViewModel() {
+        routersViewModel = RoutersViewModel(with: self,
+                                            with: CLLocationManager(),
+                                            with: mapView)
     }
 }
+
--- TeamWork-Location/Presentation/Scenes/Routers/View Model/RoutersViewModel.swift
+++ TeamWork-Location/Presentation/Scenes/Routers/View Model/RoutersViewModel.swift
@@ -6,7 +6,31 @@
 //
 
 import Foundation
+import UIKit
+import CoreLocation
+import MapKit
 
-class RoutersViewModel {
+class RoutersViewModel: NSObject, MKMapViewDelegate {
+    
+    private var globalLocations: [CLLocationCoordinate2D] = []
+    var locationManager: CLLocationManager!
+    var rootController: RoutersViewController!
+    var mapView: MKMapView!
+    
+    init(with rootController: RoutersViewController, with locationManager: CLLocationManager, with mapView: MKMapView) {
+        super.init()
+        self.locationManager = locationManager
+        self.rootController = rootController
+        self.mapView = mapView
+        configMapView()
+    }
+    
+    func configMapView() {
+        mapView.delegate = self
+    }
+    
+    
     
 }
+
+

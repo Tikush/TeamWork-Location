191d065839b5e9e4c1502f8bec07d912c52e3b1e
anamepisashvili <ana.mepisashvili@gmail.com>
2021-07-07 23:57:42 +0400

    welcome

--- TeamWork-Location/Presentation/Scenes/Welcome/Sign In/View Model/SignInViewModel.swift
+++ TeamWork-Location/Presentation/Scenes/Welcome/Sign In/View Model/SignInViewModel.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
-class SignInViewModel {
-    
+class UserDefault {
+  static var instance: UserDefault = UserDefault()
+  private init() {}
+  static func udfunc() -> UserDefaults {
+    return UserDefaults.standard
+  }
+  static func getPasswordForThisUser(usernameTextField:String) -> String {
+    return udfunc().string(forKey: usernameTextField) ?? ""
+  }
+  static func createUser(usernameTextField:String, passwordTextField:String) {
+    udfunc().set(passwordTextField, forKey: usernameTextField)
+  }
 }
